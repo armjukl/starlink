0fd3fe106713ec4abf51aec9121f6de10bc40321
kshy233 <51500612+armjukl@users.noreply.github.com>
2025-12-17 00:26:59 +0800

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,12 @@
+//// pages/index.tsx
+/*import React from 'react';
+import MusicPlayer from '../components/MusicPlayer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <MusicPlayer />
+    </div>
+  );
+}
+*/
